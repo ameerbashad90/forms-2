cf267fa7d59c16fbbfe2827f0a98fb5cd453d397
asmath <ameerbashad90@gmail.com>
2022-04-22 20:22:29 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -13,3 +13,18 @@ def insert_topic(request):
         T.save()
 
     return render(request,'insert_topic.html')
+
+def insert_webpage(request):
+    toipcs=Topic.objects.all()
+    d={'ts':'topics'}
+    if request.method=='POST':
+        tn=request.POST['topic']
+        print(tn)
+        T=Topic.objects.get_or_create(topic_name=tn)[0]
+        T.save()
+        n=request.POST['name']
+        u=request.POST['url']
+        W=Webpage.objects.get_or_create(topic_name=T,name=n,url=u)[0]
+        W.save()
+        return HttpResponse('webpage is submitted successfully')
+    return render(request,'insert_webpage.html',d)
--- pro19/urls.py
+++ pro19/urls.py
@@ -20,4 +20,5 @@ from app.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('insert_topic/',insert_topic,name='insert_topic'),
+    path('insert_webpage/',insert_webpage,name='insert_webpage'),
 ]
